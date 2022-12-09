a73c139fca23ae27a534f018b4d8284dd40915a3
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2022-12-09 16:33:49 -0300

    feat: database + connection with db

---
+++ database/db.js
@@ -0,0 +1,9 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pkg;
+
+export const connectionDB = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
 dotenv.config();
+import { connectionDB } from "../database/db.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/categories", async (req, res) => {
 
-const port = process.env.PORT || 4000;
+  try {
+    const categories = await connectionDB.query("SELECT * FROM categories");
+    res.send(categories.rows);
+  } catch (err) {
+    console.log(err)
+    res.send(err.routine);
+  }
+   
+  });
+
+const port = 4000;
 app.listen(port, () => console.log(`Server runing in port ${port}`));
