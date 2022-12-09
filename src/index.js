d48f38970210e4d7fdcd2b412996ed84305dc808
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2022-12-09 15:27:43 -0300

    feat: index.js + type: module fix

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server runing in port ${port}`));
