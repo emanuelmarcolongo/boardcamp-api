6e51437a0a30c18ffe2687769578138b4f403d34
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2022-12-10 14:52:40 -0300

    feat: improvements in getCustomerById controller

--- src/Controllers/customersController.js
+++ src/Controllers/customersController.js
@@ -1,8 +1,6 @@
 import { connectionDB } from "../database/db.js";
 
-
-
-export async function getCustomers (req, res) {
+export async function getCustomers(req, res) {
   try {
     const customers = await connectionDB.query("SELECT * FROM customers");
     res.send(customers.rows);
@@ -10,17 +8,24 @@ export async function getCustomers (req, res) {
     console.log(err);
     res.send(err.routine);
   }
-};
+}
 
-export async function getCustomersById (req, res) {
-    const {id} = req.params;
-   
+export async function getCustomersById(req, res) {
+  const { id } = req.params;
 
-    try {
-      const customers = await connectionDB.query("SELECT * FROM customers WHERE id=$1", [id]);
-      res.send(customers.rows);
-    } catch (err) {
-      console.log(err);
-      res.send(err.routine);
+  try {
+    const customers = await connectionDB.query(
+      "SELECT * FROM customers WHERE id=$1",
+      [id]
+    );
+
+    if(!customers.rows[0]) {
+        return res.sendStatus(404);
     }
-  };
+    
+    return res.send(customers.rows[0]);
+  } catch (err) {
+    
+    return res.send(err.routine);
+  }
+}
