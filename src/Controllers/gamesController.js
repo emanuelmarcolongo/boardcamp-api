5e68c80f899c3ef758ce568bc3dd01454b962f93
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2022-12-11 18:56:45 -0300

    feat: getGames shows categoryName

--- src/Controllers/gamesController.js
+++ src/Controllers/gamesController.js
@@ -2,7 +2,9 @@ import { connectionDB } from "../database/db.js";
 
 export async function getGames(req, res) {
   try {
-    const games = await connectionDB.query("SELECT * FROM games");
+    const games = await connectionDB.query(`SELECT games.*, categories.name AS "categoryName" FROM games 
+    JOIN categories 
+    ON games."categoryId" = categories.id`);
     res.send(games.rows);
   } catch (err) {
     res.sendStatus(500);
